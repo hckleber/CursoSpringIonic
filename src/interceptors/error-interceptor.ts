3a03cde89c86f9bfb2671060b427a587d84ccd6f
hc.kleber <hc.kleber@gmail.com>
2020-07-20 19:53:57 -0300

    Tratamento de erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     
-    constructor(public storage: StorageService){
+    constructor(
+        public storage: StorageService,
+        public alertCtrl : AlertController){
    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,6 +28,9 @@ export class ErrorInterceptor implements HttpInterceptor{
             console.log(errorObj);
 
             switch(errorObj.status){
+
+                case 401: 
+                this.handle401();
                 case 403:
                 this.handle403();
                 break;
@@ -36,6 +42,18 @@ export class ErrorInterceptor implements HttpInterceptor{
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text:'Ok'}
+            ]
+        });
+        alert.present();
     }
     
 }
